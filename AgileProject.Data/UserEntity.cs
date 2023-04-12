2d0e9af5b1ff8d3a1c78937093eead45ca9479e8
BaBishop1 <Babishop489@gmail.com>
2023-04-12 10:57:55 -0400

    UserEntity

---
+++ AgileProject.Data/UserEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AgileProject.Data
+{
+	public class UserEntity
+	{
+		[Key]
+		public int UserId { get; set; }
+		[Required]
+		public string Username { get; set; }
+		[Required]
+		public string Password { get; set; }
+		[Required]
+		public string Email { get; set; }
+		[Required]
+		public string Phone { get; set; }
+	}
+}
